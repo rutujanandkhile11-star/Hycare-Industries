711e0b6a7f6e2ce29d686e21716fa9b4e8b3e553
gituser2323-dev <gituser2323@gmail.com>
2026-01-23 13:22:58 +0530

    s

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ import HomePhotoSection from "./HomePhotoSection";
 import FAQComponent from "./FAQComponent";
 
 // Store (CASE SENSITIVE ✔️)
-import photoStore from "../photoStore.jsx";
+import photoStore from "../photoStore";
 
 
 // Assets
