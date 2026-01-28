c6d96c464fbe13e79792fd384c3d610d821bc255
rutujanandkhile11-star <rutujanandkhile11@gmail.com>
2026-01-28 10:49:18 +0530

    photo store

--- src/pages/Dashboard/DashboardPhotos.jsx
+++ src/pages/Dashboard/DashboardPhotos.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import photoStore from "../../store/photoStore";
+import photoStore from "../../store/photostore";
 
 
 const DashboardPhotos = () => {
